f97f1c9a00b2661ae607ef1aca6a276abe5736ce
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-06 21:50:24 -0700

    Add data

---
+++ src/data/items.js
@@ -0,0 +1,37 @@
+const items = [
+  {
+    id: 1,
+    name: "comfy slippers",
+    price: 12.00,
+    category: "Footwear",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium corporis, error, assumenda ad nemo illum sunt minima enim explicabo saepe sint ducimus itaque sit aliquid quos soluta dignissimos iusto. Corrupti, nihil minus? Laudantium cum natus quaerat necessitatibus repudiandae amet architecto dicta quae at, rem distinctio non ratione harum ex dignissimos eos odio quibusdam ab impedit? Blanditiis asperiores dolor rerum praesentium dignissimos eius nostrum debitis doloribus placeat omnis molestias voluptas, eos mollitia, nisi tempora pariatur temporibus aperiam. Est voluptatibus quo dolore nostrum praesentium repellendus nisi eligendi culpa officiis. Quidem, cumque! Libero sequi corrupti laudantium quod labore dolorem cupiditate, sint similique!"
+  },
+  {
+    id: 2,
+    name: "comfy slippers",
+    price: 12.00,
+    category: "Footwear",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium corporis, error, assumenda ad nemo illum sunt minima enim explicabo saepe sint ducimus itaque sit aliquid quos soluta dignissimos iusto. Corrupti, nihil minus? Laudantium cum natus quaerat necessitatibus repudiandae amet architecto dicta quae at, rem distinctio non ratione harum ex dignissimos eos odio quibusdam ab impedit? Blanditiis asperiores dolor rerum praesentium dignissimos eius nostrum debitis doloribus placeat omnis molestias voluptas, eos mollitia, nisi tempora pariatur temporibus aperiam. Est voluptatibus quo dolore nostrum praesentium repellendus nisi eligendi culpa officiis. Quidem, cumque! Libero sequi corrupti laudantium quod labore dolorem cupiditate, sint similique!"
+  },
+  {
+    id: 3,
+    name: "comfy slippers",
+    price: 12.00,
+    category: "Footwear",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium corporis, error, assumenda ad nemo illum sunt minima enim explicabo saepe sint ducimus itaque sit aliquid quos soluta dignissimos iusto. Corrupti, nihil minus? Laudantium cum natus quaerat necessitatibus repudiandae amet architecto dicta quae at, rem distinctio non ratione harum ex dignissimos eos odio quibusdam ab impedit? Blanditiis asperiores dolor rerum praesentium dignissimos eius nostrum debitis doloribus placeat omnis molestias voluptas, eos mollitia, nisi tempora pariatur temporibus aperiam. Est voluptatibus quo dolore nostrum praesentium repellendus nisi eligendi culpa officiis. Quidem, cumque! Libero sequi corrupti laudantium quod labore dolorem cupiditate, sint similique!"
+  },
+  {
+    id: 4,
+    name: "comfy slippers",
+    price: 12.00,
+    category: "Footwear",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium corporis, error, assumenda ad nemo illum sunt minima enim explicabo saepe sint ducimus itaque sit aliquid quos soluta dignissimos iusto. Corrupti, nihil minus? Laudantium cum natus quaerat necessitatibus repudiandae amet architecto dicta quae at, rem distinctio non ratione harum ex dignissimos eos odio quibusdam ab impedit? Blanditiis asperiores dolor rerum praesentium dignissimos eius nostrum debitis doloribus placeat omnis molestias voluptas, eos mollitia, nisi tempora pariatur temporibus aperiam. Est voluptatibus quo dolore nostrum praesentium repellendus nisi eligendi culpa officiis. Quidem, cumque! Libero sequi corrupti laudantium quod labore dolorem cupiditate, sint similique!"
+  },
+  {
+    id: 5,
+    name: "comfy slippers",
+    price: 12.00,
+    category: "Footwear",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo laudantium corporis, error, assumenda ad nemo illum sunt minima enim explicabo saepe sint ducimus itaque sit aliquid quos soluta dignissimos iusto. Corrupti, nihil minus? Laudantium cum natus quaerat necessitatibus repudiandae amet architecto dicta quae at, rem distinctio non ratione harum ex dignissimos eos odio quibusdam ab impedit? Blanditiis asperiores dolor rerum praesentium dignissimos eius nostrum debitis doloribus placeat omnis molestias voluptas, eos mollitia, nisi tempora pariatur temporibus aperiam. Est voluptatibus quo dolore nostrum praesentium repellendus nisi eligendi culpa officiis. Quidem, cumque! Libero sequi corrupti laudantium quod labore dolorem cupiditate, sint similique!"
+  }
+]
