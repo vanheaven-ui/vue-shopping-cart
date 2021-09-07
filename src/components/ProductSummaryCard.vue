6b13aeabfadb391de302e972b316cae7895df9f7
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-06 21:51:11 -0700

    Add productSummaryCard component

---
+++ src/components/ProductSummaryCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Product Summary Card</h2>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="home">
-    <h2>Home page</h2>
+    <div class="product-cards-container">
+      <ProductSummaryCard />
+    </div>
   </div>
 </template>
 
 <script>
+import ProductSummaryCard from "../components/ProductSummaryCard.vue";
+import items from "../data/items";
 export default {
   name: "Home",
-  components: {},
+  components: { ProductSummaryCard },
+  data() {
+    return {
+      items,
+    };
+  },
 };
 </script>
+<style lang="scss" scoped>
+.product-cards-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
