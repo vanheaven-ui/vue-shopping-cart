9aaf46502275648f6a2d3f0b98fd4c3204960c31
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-09-06 21:26:35 -0700

    Add general style rules

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,34 @@
   color: #2c3e50;
 }
 
+html,
+body {
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+}
+
+body {
+  background-color: rgb(245, 245, 245);
+}
+
 #nav {
   padding: 30px;
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: darkgray;
+    text-decoration: none;
+    margin: 0 5px 0 5px;
+    font-size: 1.25rem;
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &.active {
+      color: #2c3e50;
     }
   }
+}
+
+.text-center {
+  text-align: center;
 }
 </style>
